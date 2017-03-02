542a1959a09d8e0a099f1edbbe3a284345e1a684
DavadDi <dwh0403@163.com>
2017-03-02 17:51:58 +0800

    adjust ex dir

--- example/run.sh
+++ example/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "------ run ex01 ------"
-go run ex01.go
+go run ex01/ex01.go
 
 echo "------ run ex02 ------"
-go run ex02.go
+go run ex02/ex02.go
 
 echo "------ run ex03 ------"
-go run ex03.go
+go run ex03/ex03.go
