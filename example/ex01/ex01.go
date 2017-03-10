8cde76552cd73f95d3280003b865c6a53ab5b131
DavadDi <dwh0403@163.com>
2017-03-10 13:16:47 +0800

    split ex dir

---
+++ example/ex01/ex01.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DavadDi/validation"
+)
+
+// ex01 simple use
+type Person struct {
+	Name     string   `valid:"required"`
+	Email    string   `valid:"required;email"`
+	Age      int      `valid:"-"`
+	Sex      int      ``
+	WebSites []string `valid:"url"`
+}
+
+func main() {
+	web1 := "http://www.do1618.com"
+	web2 := "www.baidu.com"
+
+	person1 := &Person{
+		Name:     "dave",
+		Email:    "dwh0403@163.com",
+		WebSites: []string{web1, web2},
+	}
+
+	validater := validation.NewValidation()
+	res := validater.Validate(person1)
+
+	if res {
+		fmt.Println("Person1 validate succeed!")
+	} else {
+		fmt.Printf("Person1 validate failed. %s", validater.ErrMsg())
+	}
+
+	validater.Reset()
+	person2 := &Person{
+		Email:    "dwh0403@163.com",
+		WebSites: []string{web1, web2},
+	}
+
+	res = validater.Validate(person2)
+
+	if !res {
+		fmt.Printf("Person2 validate failed. %s\n", validater.ErrMsg())
+	} else {
+		fmt.Println("Person2 validate succeed!")
+	}
+}
---
+++ example/ex02/ex02.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DavadDi/validation"
+)
+
+type Person struct {
+	Name     string   `valid:"required"`
+	Email    string   `valid:"required;email"`
+	Age      int      `valid:"required;age"`
+	Sex      int      ``
+	WebSites []string `valid:"url"`
+}
+
+func ageChecker(v interface{}) error {
+	age, ok := v.(int)
+	if !ok {
+		return validation.NewErrWrongType("int", v)
+	}
+
+	if age <= 0 || age > 140 {
+		return fmt.Errorf("age checke failed. should between [1-140], now %d", age)
+	}
+
+	return nil
+}
+
+func main() {
+	validation.AddValidater("age", ageChecker)
+
+	person1 := &Person{
+		Name:  "dave",
+		Email: "dwh0403@163.com",
+	}
+
+	validater := validation.NewValidation()
+	res := validater.Validate(person1)
+
+	if res {
+		fmt.Println("Person1 validate succeed!")
+	} else {
+		fmt.Printf("Person1 validate failed. %s\n", validater.ErrMsg())
+	}
+}
